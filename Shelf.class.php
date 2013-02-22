9e8a287805f3a9527ec002d52c99d227cf0f4893
Lancelot HARDEL <debetux@gmail.com>
2013-02-22 23:39:08 +0100

    Cache class

---
+++ Shelf.class.php
@@ -0,0 +1,28 @@
+<?php
+	class Shelf {
+		public $useCache = true;
+		private $serverFilename;
+		private $cacheFolder;
+		private $cacheFilePath;
+		private $cacheFile;
+
+		public function __construct(){
+			$this->serverFilename = $_SERVER['SCRIPT_FILENAME'];
+			if(sizeof($_GET) > 0 || sizeof($_POST) > 0){
+				$this->useCache = false;
+			}
+			ob_start();
+		}
+
+		public function write_cache(){
+			$this->cacheFile = fopen($this->cacheFilePath, 'w');
+			fwrite($this->cacheFile, ob_get_contents());
+			fclose($this->cacheFile);
+			ob_end_flush(); # Send the output to the browser
+		}
+
+		public function read_cache(){
+			
+		}
+	}
+?>
